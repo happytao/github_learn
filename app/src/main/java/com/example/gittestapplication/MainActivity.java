5271a86ccf6aaf721f37c9e17c29ed0ffe1d9e4d
happytao <happytao.tx@gmail.com>
2021-02-23 16:12:30 +0800

    second commit

--- app/src/main/java/com/example/gittestapplication/MainActivity.java
+++ app/src/main/java/com/example/gittestapplication/MainActivity.java
@@ -3,13 +3,24 @@ package com.example.gittestapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1.findViewById(R.id.BT_exit);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
 
     }
 }
